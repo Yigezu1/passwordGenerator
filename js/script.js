f6b81f1610f2e1c5d21f1fee1c14cba6661f8dcf
Yigezu Mulugeta <Yigezumb@gmail.com>
2020-08-25 20:25:35 -0400

    updated js code

--- js/script.js
+++ js/script.js
@@ -93,21 +93,67 @@ var specialChar = [
   "}",
   "~",
 ];
-// promt user for password criteria
-var pLen = prompt("Please send valid lenght btween 8 and 128 for your password lenght");
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // promt user for password criteria
+  var pLen = parseInt(
+    prompt("Please send valid lenght btween 8 and 128 for your password lenght")
+  );
+  if (!isNaN(pLen) && pLen >= 8 && pLen <= 128) {
+    var lcase = confirm(
+      "Do you want your password to include lowercase letters?"
+    );
+    var ucase = confirm(
+      "Do you want your password to include uppercase letters?"
+    );
+    var numeric = confirm("Do you want your password to include numerics?");
+    var specialCharacter = confirm(
+      "Do you want your passowrd to include special characters?"
+    );
+    if (lcase || ucase || numeric || specialCharacter) {
+      var password = generatePassword(pLen ,lcase, ucase, numeric, specialCharacter);
+      var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+      passwordText.value = password;
+    } else {
+      var passwordText = document.querySelector("#password");
+      passwordText.value =
+        "Please choose at least one from lowercase, uppercase, numeric or special character for your password type!";
+    }
+  } else {
+    var passwordText = document.querySelector("#password");
+    passwordText.value =
+      "Please provide a valid number between 8 and 128 for your password length!";
+  }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // generatePassword function
-function generatePassword() {}
+function generatePassword(plen, lcase, ucase, numeric, specialCharacter) {
+  var joinedArray = [];
+  if(lcase){
+    joinedArray = joinedArray.concat(lowercaseL);
+  }
+  if(ucase){
+    joinedArray = joinedArray.concat(uppercaseL);
+  }
+  if(numeric){
+    joinedArray = joinedArray.concat(numerics);
+  }
+  if(specialCharacter){
+    joinedArray = joinedArray.concat(specialChar);
+  } 
+  var joinedArrayLen = joinedArray.length; 
+var pass = [];
+for(i=0; i < plen; i++){
+ pass[i] = joinedArray[Math.floor(Math.random()*joinedArrayLen)]; 
+}
+pass = pass.join('');
+return pass;
+}
