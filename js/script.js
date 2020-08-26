3ca5fcc61fdab624bd5115ecb3f1be27c34dc2fc
Yigezu Mulugeta <Yigezumb@gmail.com>
2020-08-25 20:53:55 -0400

    updated the js code

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ var uppercaseL = [
   "Y",
   "Z",
 ];
-// array of numerics
+// array of numbers
 var numerics = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // array of special characters
 var specialChar = [
